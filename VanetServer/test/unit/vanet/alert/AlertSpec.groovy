a27261a27f29a2c12b1afeb7c635882b1ce73579
Thiago Amarante <tcamarante@posgrad.ufla.br>
2014-10-25 11:08:56 -0200

    Arquivos que faltaram
    Arquivos que faltaram

---
+++ VanetServer/test/unit/vanet/alert/AlertSpec.groovy
@@ -0,0 +1,20 @@
+package vanet.alert
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Alert)
+class AlertSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
